14ce349a9de0b10976ef79fbdc6e2df52af4e5aa
daniel155158 <daniel155158@gmail.com>
2022-12-22 16:16:05 +0800

    feat: add new page

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
 app.listen(port, () => {
   console.log(`app is running in http://localhost:${port}`)
 })
