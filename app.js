a3c3764e305e9b7cb67a2c4101ba2f7acc1a727c
daniel155158 <daniel155158@gmail.com>
2022-12-22 20:45:31 +0800

    feat: add new page function via body-parser

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const port = 3000
 
 // template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// 進入首頁
 app.get('/', (req, res) => {
   res.render('index')
 })
-
+// 進入新增頁面
 app.get('/new', (req, res) => {
   res.render('new')
+})
+// 提交新增支出表單
+app.post('/new', (req, res) => {
+  const body = req.body
+  res.render('new', { body })
 })
 
 app.get('/edit', (req, res) => {
