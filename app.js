4e68dad7afcb749b18ca22bdeeaf8db3781c10d7
daniel155158 <daniel155158@gmail.com>
2022-12-22 23:18:18 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const port = 3000
 
 // template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs')
+// 非正式環境下使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+// mongoose setting
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('MongoDB connection error!')
+})
+db.once('open', () => {
+  console.log('MongoDB connected!')
+})
 
+// middleware
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // 進入首頁
