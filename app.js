f873df5b4cc6da7961e9f90070adf54460982e42
daniel155158 <daniel155158@gmail.com>
2022-12-27 11:16:30 +0800

    feat: add facebook login

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
 const app = express()
-const port = 3000
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 require('./config/mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const PORT = process.env.PORT
 
 // template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
@@ -18,7 +22,7 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
-  secret: 'MySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
 }))
@@ -34,6 +38,6 @@ app.use((req, res, next) => {
 })
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`app is running in http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`app is running in http://localhost:${PORT}`)
 })
