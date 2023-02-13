87c8c758f8922106c4db63b90a65aa1ac0817802
daniel155158 <daniel155158@gmail.com>
2023-02-13 11:27:43 +0800

    feat: modify search category function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -38,25 +38,39 @@ router.post('/search', async (req, res) => {
     const email = req.user.email
     const user = await User.findOne({ email })
     const userId = user._id
-    // 透過category的name，來取得record資料庫關聯用的category的_id
-    const search = req.body.search
-    const categoryItem = await Category.findOne({ name: search })
-    const categoryId = categoryItem._id
     // 找特定使用者的records
     const allRecords = await Record.find({ userId }).lean().sort({ date: 'desc' })
-    // 篩選出特定category的records
-    const filteredRecords = allRecords.filter(record => {
-      return (JSON.stringify(record.categoryId)) === (JSON.stringify(categoryId))
-    })
-    const records = filteredRecords.map(record => {
-      record.categoryIcon = categoryItem.icon
-      record.date = record.date.toISOString().slice(0, 10)
-      totalAmount += record.cost
-      return record
-    })
-    records.totalAmount = totalAmount
-    records.categoryName = categoryItem.name
-    res.render('index', { records })
+    // 透過category的name，來取得record資料庫關聯用的category的_id
+    const search = req.body.search
+    if (search === '全部') {
+      // 如果是篩選全部
+      const records = await Promise.all(allRecords.map(async (record) => {
+        const category = await Category.findOne({ _id: record.categoryId })
+        record.categoryIcon = category.icon
+        record.date = record.date.toISOString().slice(0, 10)
+        totalAmount += record.cost
+        return record
+      }))
+      records.totalAmount = totalAmount
+      res.render('index', { records })
+    } else {
+      // 如果是篩選個別選項
+      const categoryItem = await Category.findOne({ name: search })
+      const categoryId = categoryItem._id
+      // 篩選出特定category的records
+      const filteredRecords = allRecords.filter(record => {
+        return (JSON.stringify(record.categoryId)) === (JSON.stringify(categoryId))
+      })
+      const records = filteredRecords.map(record => {
+        record.categoryIcon = categoryItem.icon
+        record.date = record.date.toISOString().slice(0, 10)
+        totalAmount += record.cost
+        return record
+      })
+      records.totalAmount = totalAmount
+      records.categoryName = categoryItem.name
+      res.render('index', { records })
+    }
   }
   catch (error) {
     console.log(error)
